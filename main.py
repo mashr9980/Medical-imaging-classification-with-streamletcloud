9029bb251ee0a4b8ce3e0f89a2d46aa3587d4944
Muhammad aashir <46085731+mashr9980@users.noreply.github.com>
2023-08-26 23:22:50 +0500

    1st

---
+++ main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+from util import classify, set_background
+
+
+set_background('./bgs/Untitled design.png')
+
+# set title
+st.title('Skin Cancer Detection')
+
+# set header
+st.header('Please upload a sample image')
+
+# upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+# load classifier
+model = load_model('./model/Skin_Cancer.h5')
+
+# load class names
+# with open('./model/labels1.txt', 'r') as f:
+#     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+#     f.close()
+
+class_names = []
+
+with open('./model/labels1.txt', 'r') as f:
+    for line in f.readlines():
+        label = line.strip()
+        class_names.append(label)
+
+# print(class_names)
+
+
+# display image
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width=True)
+
+    # classify image
+    class_name, conf_score = classify(image, model, class_names)
+
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
---
+++ util.py
@@ -0,0 +1,76 @@
+import base64
+
+import streamlit as st
+from PIL import ImageOps, Image
+import numpy as np
+
+
+def set_background(image_file):
+    """
+    This function sets the background of a Streamlit app to an image specified by the given image file.
+
+    Parameters:
+        image_file (str): The path to the image file to be used as the background.
+
+    Returns:
+        None
+    """
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
+
+def classify(image, model, class_names):
+    """
+    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
+    score of the image.
+
+    Parameters:
+        image (PIL.Image.Image): An image to be classified.
+        model (tensorflow.keras.Model): A trained machine learning model for image classification.
+        class_names (list): A list of class names corresponding to the classes that the model can predict.
+
+    Returns:
+        A tuple of the predicted class name and the confidence score for that prediction.
+    """
+    # convert image to (224, 224)
+    image = ImageOps.fit(image, (28, 28), Image.Resampling.LANCZOS)
+
+    # convert image to numpy array
+    image_array = np.asarray(image)
+
+    # normalize image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # set model input
+    data = np.ndarray(shape=(1, 28, 28, 3), dtype=np.float32)
+    data[0] = normalized_image_array
+
+    classes = {4: ('nv', ' melanocytic nevi'),
+           6: ('mel', 'melanoma'),
+           2 :('bkl', 'benign keratosis-like lesions'),
+           1:('bcc' , ' basal cell carcinoma'),
+           5: ('vasc', ' pyogenic granulomas and hemorrhage'),
+           0: ('akiec', 'Actinic keratoses and intraepithelial carcinomae'),
+           3: ('df', 'dermatofibroma')}
+    
+    # make prediction
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    max_prob = max(prediction[0])
+    class_ind = list(prediction[0]).index(max_prob)
+    class_name = classes[class_ind]
+    # index = 0 if prediction[0][0] > 0.95 else 1
+    # class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return class_name, confidence_score
